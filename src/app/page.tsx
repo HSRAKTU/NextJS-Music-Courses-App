adfe7fb79a53bf19fb496916bec4086216fc9147
HSRAKTU <rockpriyanshu3@gmail.com>
2024-02-26 00:42:01 +0530

    Testimonial Cards added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUS from "@/components/WhyChooseUS";
 
 
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUS />
+      <MusicSchoolTestimonialCards />
     </main>
   );
 }
